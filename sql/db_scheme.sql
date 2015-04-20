720156462c2ce139b73c181f727054497ebd6654
Volodymyr Bezobiuk <dedoogun@gmail.com>
2015-04-20 18:18:10 +0300

    SQL database schema and initial data

---
+++ sql/db_scheme.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS `comments`;
+
+USE `comments`;
+
+CREATE TABLE IF NOT EXISTS `event_manager_observers` (
+  `o_id`                   INTEGER NOT NULL AUTO_INCREMENT,
+  `o_singleton_classname`  VARCHAR(256) DEFAULT NULL,
+  `o_methodname`           VARCHAR(256) NOT NULL,
+  `o_eventname`            VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`o_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `c_id`        INTEGER NOT NULL AUTO_INCREMENT,
+  `c_name`      VARCHAR(256) NOT NULL,
+  `c_comment`   TEXT NOT NULL,
+  `c_added_at`  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`c_id`),
+  KEY `c_name` (`c_name`)
+);
+
+
---
+++ sql/db_seed_data.sql
@@ -0,0 +1,5 @@
+USE `comments`;
+
+INSERT INTO `event_manager_observers`(`o_singleton_classname`, `o_methodname`, `o_eventname`)
+       VALUES (NULL, 'smilieReplace', 'submit');
+
