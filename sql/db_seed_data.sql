d5326963b3cbe2b634e54d1df5c87fe16bd10b43
Volodymyr Bezobiuk <dedoogun@gmail.com>
2015-04-24 14:21:06 +0300

    fix the initial observers db data
    code was changed not to lowercase anything, this change reflects it

--- sql/db_seed_data.sql
+++ sql/db_seed_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO `event_manager_observers`(`o_singleton_classname`, `o_methodname`, `o_eventname`)
-       VALUES (NULL, 'smilieReplace', 'submit');
+       VALUES (NULL, 'smilieReplace', 'Submit');
 
